649d100948f8390367b473a7dd95b5d1909338d4
_leodev <leonardoarencibia2002@gmail.com>
2022-08-21 19:41:26 -0400

    Created TaskList component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
+import TaskList from './components/TaskList';
 import './General.css';
-import Task from './components/Task';
 
 export default function App() {
   return (
-    <div className='task-list'>
-      <Task title="TEST1" description="mierklgsdgklsd;glmvsd;vkmfsdf;skmfvs;dv"/>
-      <Task title="TEST1" description="mierklgsdgklsd;glmvsd;vkmfsdf;skmfvs;dv"/>
-      <Task title="TEST1" description="mierklgsdgklsd;glmvsd;vkmfsdf;skmfvs;dv"/>
-      <Task title="TEST1" description="mierklgsdgklsd;glmvsd;vkmfsdf;skmfvs;dv"/>
-      <Task title="TEST1" description="mierklgsdgklsd;glmvsd;vkmfsdf;skmfvs;dv"/>
-      <Task title="TEST1" description="mierklgsdgklsd;glmvsd;vkmfsdf;skmfvs;dv"/>
-    </div>
+      <div>
+        <TaskList/>
+      </div>
   );
 }
